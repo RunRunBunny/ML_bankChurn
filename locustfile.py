094e203a19bc489ed1bf788cfaf04539d49d1c45
RunRunBunny <wzb1853396@163.com>
2023-04-24 16:14:41 -0400

    locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,33 +1,32 @@
-from locust import HttpUser, constant_pacing, task, between
 import json
-
+from locust import HttpUser, task, between
 from userInfo import userInfo
-class PretictChurn(HttpUser):
+
+class PredictChurn(HttpUser):
     host = "http://localhost:8000"
-    wait_time = constant_pacing(1)
-    
-    user_info = userInfo(
-    creditscore=600,
-    age=35,
-    tenure=5,
-    balance=10000.0,
-    numofproducts=2,
-    estimatedsalary=50000.0,
-    geography="Spain",
-    gender_Male=True,
-    hascrcard=True,
-    isactivemember=False
-    )
 
     @task
     def predict_bankChurn(self):
-      # Make the request to the API
-      response = self.client.post("/predict", json=user_info.dict())
+        user_info = userInfo(
+            creditscore=600,
+            age=35,
+            tenure=5,
+            balance=10000.0,
+            numofproducts=2,
+            estimatedsalary=50000.0,
+            geography="Spain",
+            gender_Male=True,
+            hascrcard=True,
+            isactivemember=False
+        )
 
-      # Assert that the response is successful
-      assert response.status_code == 200
+        # Make the request to the API
+        response = self.client.post("/predict", json=user_info.dict())
 
-      # Print the response content
-      print(json.loads(response.content))
+        # Print the response content
+        print(response.content)
+  
+        # Assert that the response is successful
+        assert response.status_code == 200
 
     wait_time = between(1, 5)
