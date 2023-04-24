c11c2dc2bad54db830252a1cb5aa3a128d2eea75
RunRunBunny <wzb1853396@163.com>
2023-04-24 15:43:09 -0400

    remove some comments

--- userInfo.py
+++ userInfo.py
@@ -1,19 +1,3 @@
-# from pydantic import BaseModel
-# Class which describes userInfo measurements
-# class userInfo(BaseModel):
-    # creditscore: int
-    # age: int
-    # tenure: int
-    # balance: float
-    # numofproducts: int
-    # estimatedsalary: float
-    # geography_Germany: int
-    # geography_Spain: int
-    # gender_Male: int
-    # hascrcard: int
-    # isactivemember: int
-    # userInfo: dict
-
 from pydantic import BaseModel, validator
 
 class userInfo(BaseModel):
